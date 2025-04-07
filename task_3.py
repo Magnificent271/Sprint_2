1ddaa73e586b7673d25dd31359ad70c28dcfaacd
Magnificent271 <magnificent271@gmail.com>
2025-04-07 21:13:29 +0300

    внеМетоды get_points_for_place и get_points_for_meters сделать статичными

--- task_3.py
+++ task_3.py
@@ -1,28 +1,23 @@
 class PointsForPlace:
 
-    def __init__(self):
-        self.points = 0
-
-    def get_points_for_place(self, place):
+    @staticmethod
+    def get_points_for_place(place):
         if place > 100:
             return f"Баллы начисляются только первым 100 участникам"
         elif place < 1:
             return f"Спортсмен не может занять нулевое или отрицательное место"
         else:
-            self.points = 101 - place
-            return self.points
+            return 101 - place
 
 
 class PointsForMeters:
-    def __init__(self):
-        self.points = 0
 
-    def get_points_for_meters(self, meters):
+    @staticmethod
+    def get_points_for_meters(meters):
         if meters < 0:
             return f'Количество метров не может быть отрицательным'
         else:
-            self.points = meters * 0.5
-            return self.points
+            return meters * 0.5
 
 
 class TotalPoints(PointsForPlace, PointsForMeters):
